14a3737e0ef3880a30d6c8b3a6afa87c787a97b0
vainawebm <vainaweb@gmail.com>
2020-09-18 16:18:09 -0300

    cubo 3d feito, falta fazer animação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-cube">
+        <div className="cubespin">
+          <div className="face1">1</div>
+          <div className="face2">2</div>
+          <div className="face3">3</div>
+          <div className="face4">4</div>
+          <div className="face5">5</div>
+          <div className="face6">6</div>
+        </div>
+      </div>
     </div>
   );
 }
